d19d9cdc7b5ceec842f2588131053c2965667eda
AYO <whdkdy02@naver.com>
2024-06-10 18:50:37 +0900

    MuzzleFlashes 추가 & 일부 필요없는 것 삭제

---
+++ Assets/KriptoFX/MuzzleFlashes/Scripts/CameraSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCU
+{
+    public class CameraSystem : MonoBehaviour
+    {
+        public static CameraSystem instance { get; private set; } = null;
+
+        // public bool IsTPSMode { get {return isTPSMode;} }
+
+        /// <summary>
+        /// 현재 CameraSystem이 TPS모드로 작동하고 있는지 여부 값
+        /// </summary>
+        public bool IsTPSMode => isTPSMode;
+        public float TargetFOV { get; set; } = 60.0f;
+
+        public Cinemachine.CinemachineVirtualCamera tpsCamera;
+        public Cinemachine.CinemachineVirtualCamera fpsCamera;
+
+        public float zoomSpeed = 5.0f;
+
+        private bool isTPSMode = true;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+        private void Start()
+        {
+            isTPSMode = tpsCamera.gameObject.activeSelf;
+        }
+
+        private void OnDestroy()
+        {
+            instance = null;
+
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                isTPSMode = !isTPSMode;
+                tpsCamera.gameObject.SetActive(isTPSMode);
+                fpsCamera.gameObject.SetActive(!isTPSMode);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            tpsCamera.m_Lens.FieldOfView = Mathf.Lerp(tpsCamera.m_Lens.FieldOfView, TargetFOV, zoomSpeed * Time.deltaTime);
+            fpsCamera.transform.forward = fpsCamera.Follow.transform.forward;
+        }
+    }
+}
---
+++ Assets/KriptoFX/MuzzleFlashes/Scripts/Weapon.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCU
+{
+    public class Weapon : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(firePosition.position, firePosition.position + firePosition.forward * range);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Camera.main.transform.forward * range);
+        }
+
+        public GameObject MuzzlePrefab;
+        public GameObject bulletPrefab;
+        public Transform firePosition;
+
+        public float fireRate = 0.1f;
+        public float range = 100f;
+
+        private float lastShootTime = 0f;
+
+        private void Update()
+        {
+            
+        }
+
+        public void Shoot()
+        {
+            if (Time.time > lastShootTime + fireRate)
+            {
+                //Possible Shoot
+
+                lastShootTime = Time.time;
+
+                //create bullet
+                var newMuzzle = Instantiate(MuzzlePrefab);
+                newMuzzle.transform.SetPositionAndRotation(firePosition.position, firePosition.rotation);
+                newMuzzle.gameObject.SetActive(true);
+                Destroy(newMuzzle, 1f);
+
+                // Create Bullet
+                var newBullet = Instantiate(bulletPrefab);
+                //총에서 총알이 나가는 것으로 변경(카메라 -> 총구)
+                newBullet.transform.SetPositionAndRotation(firePosition.position, firePosition.rotation);
+                newBullet.gameObject.SetActive(true);
+
+                //두 오브젝트가 서로 충돌처리가 되지 않게 Unity Physics Engine에게 무시하도록 지시
+                Physics.IgnoreCollision(newBullet.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+            }
+        }
+    }
+}
