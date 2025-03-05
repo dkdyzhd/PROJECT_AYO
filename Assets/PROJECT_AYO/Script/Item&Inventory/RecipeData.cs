feb68397d39f2308af1101591fac72612c5a7e81
AYO\우리아요 <whdkdy02@naver.com>
2025-03-05 12:03:12 +0900

    크래프팅 시스템을 위한 크래프팅 데이터 설계중 _ RecipeData 스크립트 생성

---
+++ Assets/PROJECT_AYO/Script/Item&Inventory/RecipeData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AYO
+{
+    [CreateAssetMenu(fileName = "New Recipe", menuName = "Crafting")]
+    public class RecipeData : ScriptableObject
+    {
+        public ItemData outputItem; //제작 결과물
+        public int outputQuantity;  //제작 결과물의 개수
+
+        //필요한 재료 구조체
+        [System.Serializable]
+        public struct Ingredient
+        {
+            public Item item;   //재료가 되는 아이템
+            public int quantity;    //필요한 개수
+        }
+
+        public Ingredient[] ingredients;    //제작에 필요한 재료 목록을 저장할 리스트
+
+    }
+}
