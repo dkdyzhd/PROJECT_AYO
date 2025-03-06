89995e3c927f4ab9e59a625be6511d4f7d062371
AYO\우리아요 <whdkdy02@naver.com>
2025-03-06 22:37:40 +0900

    CraftingManager 스크립트 작성

--- Assets/PROJECT_AYO/Script/Item&Inventory/CraftingManager.cs
+++ Assets/PROJECT_AYO/Script/Item&Inventory/CraftingManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AYO
@@ -7,12 +8,33 @@ namespace AYO
     public class CraftingManager : MonoBehaviour
     {
         public RecipeData[] recipes;
-        public QuickSlotController QuickSlotController;
+        public QuickSlotController quickSlotController;
 
-        //public bool CraftItem(RecipeData recipe) //인벤토리에 아이템이 있는지 검사
-        //{
-        //    recipe.ingredients
-        //}   
+        public bool CraftItem(RecipeData recipe) // 인벤토리에 아이템이 있는지 검사
+        {
+            // 필요한 아이템이 없을 경우
+            foreach (var ingredient in recipe.ingredients)
+            {
+                if(!quickSlotController.HasItem(ingredient.item, ingredient.quantity))
+                {
+                    Debug.Log("재료가 부족합니다!");
+                    return false;
+                }
+            }
+
+            // 필요한 아이템이 있는 경우
+            foreach(var ingredient in recipe.ingredients)
+            {
+                // 재료 차감
+                quickSlotController.RemoveItem(ingredient.item, ingredient.quantity);
+            }
+
+            // 퀵슬롯에 제작결과물 추가
+            quickSlotController.AddItem(recipe.outputItem);
+            Debug.Log($"제작완료 {recipe.outputItem.itemName}");
+
+            return true;
+        }
 
     }
 }
