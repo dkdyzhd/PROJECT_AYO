c650fc1caad26cc4089fe61a015f13eb6a2d0036
D18\D18 <whdkdy02@naver.com>
2025-03-10 15:52:25 +0900

    CraftingManager에 availableRecipes 리스트 추가 & 판별 및 제작 기능 넣음 **UI에 버튼 미리 다 만들어 놓고 보여지는 것만 하게 수정하기

--- Assets/PROJECT_AYO/Script/Item&Inventory/CraftingManager.cs
+++ Assets/PROJECT_AYO/Script/Item&Inventory/CraftingManager.cs
@@ -2,14 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static AYO.RecipeData;
 
 namespace AYO
 {
     public class CraftingManager : MonoBehaviour
     {
-        public RecipeData[] recipes;
+        //public List<RecipeData> recipes;    //배열에서 리스트로 변경_25.03.10
         public QuickSlotController quickSlotController;
 
+        public List<RecipeData> availableRecipes;
+        public CraftingUI craftingUI;
+
+        // 제작가능한지 판별
+        public bool TestCanCraftItem(RecipeData recipe)
+        {
+            foreach (RecipeData.Ingredient ingredient in recipe.ingredients)
+            {
+                if (!quickSlotController.HasItem(ingredient.item, ingredient.quantity))
+                {
+                    return false;
+                }
+            }
+
+            // 필요한 아이템이 있는 경우
+            foreach (var ingredient in recipe.ingredients)
+            {
+                TestCraftItem(recipe);
+            }
+            return true;
+        }
+
+        // 제작
+        public void TestCraftItem(RecipeData recipe)
+        {
+            // 퀵슬롯에 제작결과물 추가
+            quickSlotController.AddItem(recipe.outputItem);
+            Debug.Log($"제작완료 {recipe.outputItem.itemName}");
+        }
+
+        // -------------------------------------------------------
         public bool CraftItem(RecipeData recipe) // 인벤토리에 아이템이 있는지 검사
         {
             //foreach문 사용이유 = for문을 간단하게 사용 & Dictionary는 for문을 사용하지 못함
--- Assets/PROJECT_AYO/Script/Item&Inventory/CraftingUI.cs
+++ Assets/PROJECT_AYO/Script/Item&Inventory/CraftingUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AYO
 {
@@ -9,29 +10,67 @@ namespace AYO
         public CraftingManager craftingManager;
         public GameObject quickSlotController;
         [HideInInspector]
-        public RecipeData selectRecipe;
+        // 아이템을 선택하고 제작 버튼을 따로 만들었을때
+        //public RecipeData selectRecipe;
+
+        //public Transform itemListParent;    //UI아이템 목록이 들어갈 부모 오브젝트
+        //public GameObject itemButtonPrefab;
+        // 리스트형식으로 제작가능한 아이템 목록만들어서 아이템을 선택하면 제작이 되도록
+        //public List<RecipeData> availableRecipes;   
 
         // Start is called before the first frame update
         void Start()
         {
+            
+        }
+
+        //void CreateCraftingItem()   // CraftingUI에 제작가능한 아이템 목록 버튼 생성
+        //{
+        //    foreach (var recipe in availableRecipes)
+        //    {
+        //        GameObject itemButton = Instantiate(itemButtonPrefab, itemListParent);  // 버튼 생성
+        //        // To do : 아이템 이미지 불러오기
+        //        // 아이템 이름 불러오기
+        //        Text buttonText = itemButton.GetComponentInChildren<Text>();
+        //        buttonText.text = recipe.outputItem.itemName;
+
+        //        Button button = itemButton.GetComponent<Button>();
+        //        button.onClick.AddListener(() => OnItemClicked(recipe));
+
+        //        // 재료가 부족하면 버튼 비활성화
+        //        if (!craftingManager.CraftItem(recipe))
+        //        {
+        //            button.interactable = false;
+        //            buttonText.color = Color.gray; // 비활성화 색상 변경
+        //        }
+        //    }
+        //}
         
-        }
+        //void OnItemClicked(RecipeData recipe)   // 클릭시 제작
+        //{
+        //    bool craftSuccess = craftingManager.CraftItem(recipe);
+        //    if (craftSuccess)
+        //    {   //CraftingManager 에서 제작이 되면
+        //        Debug.Log($"{recipe.outputItem.itemName} 제작 성공!");
+        //    }
+        //}
 
-        public void OnClickCraftButton()
-        {
-            TryCraftItem();
-        }
-
-        void TryCraftItem()
-        {
-            if(selectRecipe != null)
-            {
-                bool craftSuccess = craftingManager.CraftItem(selectRecipe);
-                if (craftSuccess)
-                {
-                    Debug.Log($"{selectRecipe.outputItem.itemName} 제작 성공!");
-                }
-            }
-        }
+        ////예비
+        //public void OnClickCraftButton()
+        //{
+        //    TryCraftItem();
+        //}
+        ////예비
+        //void TryCraftItem()
+        //{
+        //    if(selectRecipe != null)
+        //    {
+        //        bool craftSuccess = craftingManager.CraftItem(selectRecipe);
+        //        if (craftSuccess)
+        //        {
+        //            Debug.Log($"{selectRecipe.outputItem.itemName} 제작 성공!");
+        //        }
+        //    }
+        //}
     }
 }
